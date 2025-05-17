cf924ee45af73b01079a4e112835267a4de6f667
JeffSSC <jeff.silva.caires@gmail.com>
2025-05-17 14:28:16 -0300

    feat(main.py): Adicionando versão inicial da API

---
+++ main.py
@@ -0,0 +1,37 @@
+import base64
+import os
+from openai import OpenAI
+from PIL import Image
+from io import BytesIO
+from IPython.display import Image as IPImage, display
+from config import OPENAI_API_KEY
+
+client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY", OPENAI_API_KEY))
+
+# Create imgs/ folder
+folder_path = "imgs"
+os.makedirs(folder_path, exist_ok=True)
+
+prompt_edit = "Coloque uma cabeça de panda igual ao da codecon nessa pessoa."
+img_path_edit = "./teste.jpg"
+
+img1 = open("./fabiano.jpg", "rb")
+img2 = open("./teste_panda.png", "rb")
+
+# Generate the new image
+result_edit = client.images.edit(
+    model="gpt-image-1",
+    image=[img1,img2], 
+    prompt=prompt_edit,
+    size="1024x1536"
+)
+
+# Save the image to a file and resize/compress for smaller files
+image_base64 = result_edit.data[0].b64_json
+image_bytes = base64.b64decode(image_base64)
+
+image = Image.open(BytesIO(image_bytes))
+image.save(img_path_edit, format="JPEG", quality=80, optimize=True)
+
+# Show the result
+display(IPImage(img_path_edit))
